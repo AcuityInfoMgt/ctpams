18d526e1737641c55ff63f0e64b80e62b726b5c4
Daniel Jordan <djd123@gmail.com>
2015-04-07 07:32:36 -0700

    update doc links

--- app/models/attached_file.rb
+++ app/models/attached_file.rb
@@ -1,7 +1,7 @@
 class AttachedFile < ActiveRecord::Base
   has_attached_file :attachable_document
   validates_with AttachmentPresenceValidator, :attributes => :attachable_document
-  validates_attachment_file_name :attachable_document, :matches => [/txt\Z/, /odt\Z/, /pdf\Z/]
+  validates_attachment_file_name :attachable_document, :matches => [/ods\Z/, /odt\Z/, /pdf\Z/]
   #validates_with AttachmentSizeValidator, :attributes => :attachable_document, :less_than => 1.megabytes
   belongs_to :attachable, polymorphic: true
   after_initialize :set_defaults, :if => :new_record?
