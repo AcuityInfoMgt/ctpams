e37ba2a967ee9bd59c56b38daaabecb2202f94fb
Daniel Jordan <djd123@gmail.com>
2015-04-14 13:20:06 -0700

    project workload summary done

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,49 +20,59 @@ class User < ActiveRecord::Base
       @projects = Project.where(workflow_state: 'proposal_in_progress').where(user_id: self.id)
     elsif self.role == 'reviewer'
         @projects = Project.where(workflow_state: 'proposal_in_progress').where(program_id: self.programs).where("id in (select project_id from countries_projects where country_id in (select country_id from countries_regions where region_id in (select region_id from regions_users where user_id = (?))))", self.id)
-    elsif self.role == 'budget'
+    elsif self.role == 'admin'
       @projects = Project.where(workflow_state: 'proposal_in_progress')
     end
   end
 
   def get_preliminary_program_review
-    #@role = self.role
-    if self.role = :admin
+    if self.role == 'reviewer'
+      @projects = Project.where(workflow_state: 'preliminary_program_review').where(program_id: self.programs).where("id in (select project_id from countries_projects where country_id in (select country_id from countries_regions where region_id in (select region_id from regions_users where user_id = (?))))", self.id)
+    elsif self.role == 'admin'
       @projects = Project.where(workflow_state: 'preliminary_program_review')
-    else
-      nil
     end
-
   end
 
   def get_pre_legal_review
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'pre_legal_review')
+    if self.role == 'admin' || self.role == 'legal'
+      @projects = Project.where(workflow_state: 'pre_legal_review')
+    end
   end
 
   def get_regional_review
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'regional_review')
+    if self.role == 'regional'
+      @projects = Project.where(workflow_state: 'regional_review').where(program_id: self.programs).where("id in (select project_id from countries_projects where country_id in (select country_id from countries_regions where region_id in (select region_id from regions_users where user_id = (?))))", self.id)
+    elsif self.role == 'admin'
+      @projects = Project.where(workflow_state: 'regional_review')
+    end
   end
 
   def get_secondary_program_review
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'secondary_program_review')
+    if self.role == 'reviewer'
+      @projects = Project.where(workflow_state: 'secondary_program_review').where(program_id: self.programs).where("id in (select project_id from countries_projects where country_id in (select country_id from countries_regions where region_id in (select region_id from regions_users where user_id = (?))))", self.id)
+    elsif self.role == 'admin'
+      @projects = Project.where(workflow_state: 'secondary_program_review')
+    end
   end
 
   def get_cn_clearance_pending
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'cn_clearance_pending')
+    if self.role == 'reviewer'
+      @projects = Project.where(workflow_state: 'cn_clearance_pending').where(program_id: self.programs).where("id in (select project_id from countries_projects where country_id in (select country_id from countries_regions where region_id in (select region_id from regions_users where user_id = (?))))", self.id)
+    elsif self.role == 'admin' || self.role == 'budget'
+      @projects = Project.where(workflow_state: 'cn_clearance_pending')
+    end
   end
 
   def get_funding_clearance_pending
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'funding_clearance_pending')
+    if self.role == 'admin' || self.role == 'budget'
+      @projects = Project.where(workflow_state: 'funding_clearance_pending')
+    end
   end
 
   def get_obligation_pending
-    #@role = self.role
-    @projects = Project.where(workflow_state: 'obligation_pending')
+    if self.role == 'admin' || self.role == 'budget'
+      @projects = Project.where(workflow_state: 'obligation_pending')
+    end
   end
 
 
